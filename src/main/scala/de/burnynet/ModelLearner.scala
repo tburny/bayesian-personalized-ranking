8312ce8fe623ab1480b911e93beed96da6b0e0b9
Tobias Brennecke <t.burny@gmail.com>
2013-05-25 17:02:25 +0200

    use a foldLeft in LearnBpr

--- src/main/scala/de/burnynet/ModelLearner.scala
+++ src/main/scala/de/burnynet/ModelLearner.scala
@@ -13,24 +13,18 @@ object ModelLearner {
 
   // Use symbols as in the paper, thats more readable
   def learnBpr(α :Double, λ:Double, model: BPRModel) = {
-    def recurse(ds:List[(Int,Int,Int)], Θ: DenseMatrix[Double]) : DenseMatrix[Double] = {
-      ds match {
-        // TODO check this
-        case List() => Θ
-        // Maybe not just: Define convergence as "all items picked from ds"
-        case (u,i,j) :: xs =>
-          // e ^ - x_uij
-          val exp_X_uij = exp(-model.xUIJ(u,i,j))
-          // Θ = Θ +  α * (e^-x_uij / (1+ e^-x_uij ) * d/dΘ (x_uij) + Θ * λ)
-          recurse(xs, Θ) + (((model.deriveXUIJ(u,i,j) :* (exp_X_uij / (1+exp_X_uij))) + (Θ :* λ)) * α)
-      }
-
-    }
-
     val Θ = model.correlationMatrix.copy
+    // randomize which items of D_s are picked first
     val rndDs = Random.shuffle(model.Ds).toList
-
-    recurse(rndDs, Θ)
+    // just do it for all items.
+    // See e.g. /org/recommender101/recommender/extensions/bprmf/BPRMFRecommender.java:212 in Recommender101
+    rndDs.foldLeft(Θ) {
+      case (akku, (u,i,j)) =>
+        // e ^ - x_uij
+        val exp_X_uij = exp(-model.xUIJ(u,i,j))
+        // Θ = Θ +  α * (e^-x_uij / (1+ e^-x_uij ) * d/dΘ (x_uij) + Θ * λ)
+        akku + (((model.deriveXUIJ(u,i,j) :* (exp_X_uij / (1+exp_X_uij))) + (Θ :* λ)) * α)
+    }
   }
 
 }
